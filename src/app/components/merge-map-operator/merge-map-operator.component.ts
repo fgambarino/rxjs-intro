e8073443e4b0d843c6ab850dcb6394e17aaa3a7e
fgambarino <30149840+fgambarino@users.noreply.github.com>
2020-05-20 13:40:43 +0200

    More meaningful values on maps (#10)
    * a little improvement in styles
    
    * concat letters / number is a better choice than arithmetic
    
    Co-authored-by: Federico Gambarino <federico.gambarino@deltatre.it>

--- src/app/components/merge-map-operator/merge-map-operator.component.ts
+++ src/app/components/merge-map-operator/merge-map-operator.component.ts
@@ -5,7 +5,7 @@ import { map, mergeMap, take } from 'rxjs/operators';
 @Component({
   selector: 'app-merge-map-operator',
   templateUrl: './merge-map-operator.component.html',
-  styleUrls: ['./merge-map-operator.component.scss']
+  styleUrls: ['./merge-map-operator.component.scss'],
 })
 export class MergeMapOperatorComponent implements OnInit {
   sourceObservable$: Observable<any>;
@@ -17,15 +17,15 @@ export class MergeMapOperatorComponent implements OnInit {
   ngOnInit() {
     this.sourceObservable$ = interval(1400).pipe(
       take(3),
-      map(i => i + 10)
+      map((i) => i.toString())
     );
     this.sourceObservable2$ = interval(1800).pipe(
       take(3),
-      map(i => i + 1)
+      map((i) => 'ABC'[i])
     );
     this.newObservable$ = this.sourceObservable$.pipe(
-      mergeMap(outer =>
-        this.sourceObservable2$.pipe(map(inner => inner * outer))
+      mergeMap((outer) =>
+        this.sourceObservable2$.pipe(map((inner) => inner + outer))
       )
     );
   }
